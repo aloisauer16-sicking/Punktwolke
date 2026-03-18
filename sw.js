81c93807ef50ec6dfc8a0bc85928f509de33036b
aloisauer16-sicking <aloisauer16@gmail.com>
2026-03-18 09:58:49 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,15 @@
-const CACHE = 'punktwolke-v4';
+const CACHE = 'punktwolke-v5';
 const FILES = ['/', '/index.html', '/manifest.json'];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
 });
